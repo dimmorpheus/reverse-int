33303ae0a7155cd2d32c085eb80a6a6830b47ee9
dimmorpheus <dimmorpheus@icloud.com>
2021-03-14 10:50:00 +0300

    init: implement reverseInt function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+	var str;
+	var revStr;
+
+	str = n.toString();
+	revStr = "";
+
+	for (var i = str.length - 1; i >= 0; --i)
+		revStr += str[i];
+
+	return parseInt(revStr);
   
 }
